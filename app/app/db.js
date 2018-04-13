5b4449c450be70d27d4876bee408756cbb19defd
Yuhui Xiong <Yuhui.Xiong@mcon-group.com>
2018-04-13 11:48:11 +0200

    add hint for mongoose connection

--- app/app/db.js
+++ app/app/db.js
@@ -2,4 +2,5 @@
 
 const mongoose = require('mongoose');
 
+// check for more details about connection on offcicial documentation, https://github.com/Automattic/mongoose#connecting-to-mongodb
 // mongoose.connect('mongodb://localhost/Tododb');
