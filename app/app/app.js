b422d4e43bc4d08f3f00228958ffd6e48051f3e2
Yuhui Xiong <Yuhui.Xiong@mcon-group.com>
2018-04-15 17:45:07 +0200

    add helmet

--- app/app/app.js
+++ app/app/app.js
@@ -6,6 +6,9 @@ require('./db');
 const express = require('express');
 const app = express();
 
+const helmet = require('helmet');
+app.use(helmet());
+
 const router = express.Router();
 const bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({extended: true}));
